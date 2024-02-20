5e4427108a64cb280bad06b7c8e369211a09b5c7
vccyb <13170906656@163.com>
2024-02-20 20:05:56 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import path from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
